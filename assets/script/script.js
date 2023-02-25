d4d382c196f514f8c35900faa7c64cf14148b065
Hoàng Anh <nth.anh020209@gmail.com>
2023-02-25 14:05:57 +0700

    edit js for modal

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,9 @@
+const login = document.querySelector(".auth-login");
+const signup = document.querySelector(".auth-signup");
+const modal = document.querySelector(".modal");
+
 function openModal () {
     document.querySelector('.modal').style.display = "flex"; 
-}
-
-function closeModal () {
-    document.querySelector('.modal').style.display = "none";
 }
 
 function openLoginForm() { 
@@ -12,25 +12,23 @@ function openLoginForm() {
   
 function openSignupForm() { 
     document.querySelector(".auth-signup").style.display = "block"; 
-} 
-  
-function closeLoginForm() { 
-    document.querySelector(".auth-login").style.display = "none"; 
-} 
-  
-function closeSignupForm() { 
-    document.querySelector(".auth-signup").style.display = "none"; 
 }
 
-// function toggleForms(){ 
-//     if(document.querySelector(".auth-login").style.display == "block") { 
-//       document.querySelector(".auth-login").style.display = "none"; 
-//     } 
-//     else if(document.querySelector(".auth-signup").style.display == "block") { 
-//       document.querySelector(".auth-signup").style.display = "none"; 
-//     } 
-//     else { 
-//       document.querySelector(".auth-login").style.display = "block"; 
-//       document.querySelector(".auth-signup").style.display = "block"; 
-//     } 
-// }
+// Đóng tất cả form khi ấn vào vùng trống
+function closeAllForm() {
+    document.querySelector(".auth-login").style.display = "none";
+    document.querySelector(".auth-signup").style.display = "none"; 
+    document.querySelector(".modal").style.display = "none";
+}
+
+// Chuyển đổi giữa các form
+function swichForms() {
+    if (document.querySelector(".auth-login").style.display == "block") {
+        document.querySelector(".auth-login").style.display = "none";
+        document.querySelector(".auth-signup").style.display = "block";
+    }
+    else if (document.querySelector(".auth-signup").style.display == "block" ) {
+        document.querySelector(".auth-signup").style.display = "none";
+        document.querySelector(".auth-login").style.display = "block";
+    }
+}
