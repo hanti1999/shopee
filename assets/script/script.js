20670fc4bd783ebf56d0b569b39d05ff7c4c6133
Hoàng Anh <nth.anh020209@gmail.com>
2023-02-28 19:09:22 +0700

    add more js

--- assets/script/script.js
+++ assets/script/script.js
@@ -25,3 +25,42 @@ function swichForms() {
         document.querySelector(".auth-login").style.display = "block";
     }
 }
+
+
+// Cách pro hơn với jQuery
+// $(document).ready(function() {
+//     // Ẩn modal khi tải trang
+//     $('.modal').hide();
+    
+//     // Xử lý sự kiện khi click vào nút Login
+//     $('#login-btn').click(function() {
+//         $('.modal').show().css("display","flex");
+//         $('.auth-login').show();
+//         $('.auth-signup').hide();
+//     });
+    
+//     // Xử lý sự kiện khi click vào nút Register
+//     $('#register-btn').click(function() {
+//         $('.modal').show().css("display","flex");
+//         $('.auth-signup').show();
+//         $('.auth-login').hide();
+//     });
+    
+//     // Xử lý sự kiện khi click vào nút đóng modal hoặc khoảng trống
+//     $('.close').click(function() {
+//         $('.modal').hide();
+//     });
+//     $('.modal__overlay').click(function() {
+//         $('.modal').hide();
+//     });
+
+//     // Xử lý sự kiện khi chuyển đổi form
+//     $('#switch-to-login').click(function() {
+//         $('.auth-signup').hide();
+//         $('.auth-login').show();
+//     })
+//     $('#switch-to-register').click(function() {
+//         $('.auth-signup').show();
+//         $('.auth-login').hide();
+//     })
+// });
